f839512061c2c60206a8e51c3b08e40f3f6d5e93
talkin-ai <bm.ai@posteo.net>
2023-06-20 16:18:27 -0700

    Update portal.js

--- pages/api/payment/portal.js
+++ pages/api/payment/portal.js
@@ -1,14 +1,26 @@
 
+
+import initStripe from "stripe";
+
 import { getToken } from 'next-auth/jwt';
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+//import { getToken } from 'next-auth/jwt';
 
+import prisma from "../../../lib/prisma";
+
+
+const stripe = initStripe(process.env.STRIPE_SECRET_KEY);
 
 
 
 export default async function handle(req, res) {
 
+
+
   const token = await getToken({ req });
+
+  //const productId = 'price_1Mw7T1Dfv2951nlDXi5SR4Fe';
+
 
   const user = await prisma.user.findUnique({
     where: {
@@ -16,13 +28,16 @@ export default async function handle(req, res) {
     },
   });
 
-  const session = await stripe.billingPortal.sessions.create({
+
+
+
+  const stripe_session = await stripe.billingPortal.sessions.create({
     customer: user.stripeCustomerId,
     return_url: process.env.CLIENT_URL,
   })
 
   res.send({
-    url: session.url,
+    url: stripe_session.url,
   })
 
 }
