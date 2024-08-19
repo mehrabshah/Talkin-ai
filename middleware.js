8e71b7a8e81ac63a0933c9122cc20465fea7cec5
talkin-ai <bm.ai@posteo.net>
2024-08-19 00:09:09 -0700

    update middleware

---
+++ middleware.js
@@ -0,0 +1,16 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+export default authMiddleware({
+  // Set the paths that don't require the user to be signed in
+  // Sign in and sign up pages are already made public by Clerk
+  publicRoutes: ["/"],
+  //ignoredRoutes: ["/((?!api|trpc))(_next|.+\..+)(.*)", "/img2video"]
+});
+
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)","/","/(api|trpc)(.*)"],
+};
+
+
+
