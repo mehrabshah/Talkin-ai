dc59ec21532defeed487839f768470e63a764d9c
talkin-ai <bm.ai@posteo.net>
2024-09-08 11:03:56 -0700

    update banner.js

--- data/banner.js
+++ data/banner.js
@@ -1,97 +1,50 @@
 export const bannerLeftVideos = [
   {
-    src: "/assets/videos/A_boy_with_lantern.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/1.jpg",
+    src: "/assets/videos/A_magical_circus.mp4",
+    
   },
+  
   {
-    src: "/assets/videos/A_charming_fox.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
-  },
-  {
-    src: "/assets/videos/A_fantastical_garden.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/1.jpg",
+    src: "/assets/videos/A_magical_forest.mp4",
+   
   },
   {
     src: "/assets/videos/A_futuristic_art_galary.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
+   
   },
-  {
-    src: "/assets/videos/A_futuristic_safari.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/1.jpg",
-  },
-  {
-    src: "/assets/videos/A_hidden_glade_harry.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
-  },
+  
+ 
   {
     src: "/assets/videos/A_magical_study-vmake.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
+    
   },
-  {
-    src: "/assets/videos/A_neon-lit_metropolis.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
-  },
+  
   {
     src: "/assets/videos/A_secret_chamber-vmake.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
+    
   },
+  
   // Add more videos as needed
 ];
 
 export const bannerRightVideos = [
   {
-    src: "/assets/videos/A_hidden_glade_harry_v2.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/1.jpg",
+    src: "/assets/videos/A_hidden_glade_harry_v2.mp4",  
   },
   {
     src: "/assets/videos/A_hidden_jungle.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
   },
+  
+  {
+    src: "/assets/videos/A_futuristic_safari.mp4",
+  },
+ 
   {
     src: "/assets/videos/A_hidden_library-vmake.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/1.jpg",
-  },
-  {
-    src: "/assets/videos/A_magical_circus.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
-  },
-  {
-    src: "/assets/videos/A_magical_forest.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/1.jpg",
-  },
-  {
-    src: "/assets/videos/A_magical_forest_2.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
-  },
-  {
-    src: "/assets/videos/A_vibrant_futurist_city.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
   },
   {
     src: "/assets/videos/A_whimsical_school_kid.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
   },
-  {
-    src: "/assets/videos/Girl_poterry.mp4",
-    poster:
-      "https://cdn-luma.com/public/lumalabs.ai/june-12-launch-dream-machine-high-res/assets/dream-machine/featured/thumbnails/2.jpg",
-  },
+  
   // Add more videos as needed
 ];
