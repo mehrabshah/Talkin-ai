e06e5e34e7f299cc48f1942eb2f122032d4731f5
talkin-ai <bm.ai@posteo.net>
2024-09-09 23:38:17 -0700

    update middleware

--- middleware.ts
+++ middleware.ts
@@ -3,17 +3,13 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   // Set the paths that don't require the user to be signed in
   // Sign in and sign up pages are already made public by Clerk
-<<<<<<< HEAD
-  publicRoutes: ["/", "/contact", "/about", "/privacy-policy", "/api/webhook", '/api/elevenlabs'],
-=======
-  publicRoutes: ["/", "/contact", "/about", "/privacy-policy", "/api/webhook", "/api/elevenlabs"],
->>>>>>> da469d7a616198b9de4feeaa27e890c8398383cb
+  publicRoutes: ['/', '/contact', '/about', '/privacy-policy'],
 });
-
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)","/","/(api|trpc)(.*)"],
 };
+
 
 
 
